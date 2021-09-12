ed8e1bb9e6a96967b40308549f4e54c21f98db7b
NicolasOCruz <nicolaspereira244@gmail.com>
2021-09-12 01:54:23 -0300

    Servidor de Configuração

---
+++ hr-config-server/src/test/java/com/nicolascruz/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolascruz.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
